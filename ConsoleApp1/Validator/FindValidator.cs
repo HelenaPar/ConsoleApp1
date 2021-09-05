095adef4bd6194f8a05c08fc0bfaf3d1572461bb
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-09-05 17:49:46 +0300

    Add files via upload

---
+++ ConsoleApp1/Validator/FindValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Validator
+{
+    class FindValidator : BaseValidator
+    {
+        public FindValidator(string[] parameters) : base(parameters)
+        {
+
+        }
+        public override bool Validate()
+        {
+            return ValidateNameOrSurname(parameters[0]);
+        }
+    }
+}
---
+++ ConsoleApp1/Validator/IValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Validator
+{
+    public interface IValidator
+    {
+        public bool Validate();
+        public string ErrorMessage { get; set; }
+    }
+}
