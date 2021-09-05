408f9cff16d9d73e0d12e870d760082e84ce38b7
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-09-05 14:13:24 +0300

    Add files via upload

---
+++ ConsoleApp1/Command/UnknownCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Command
+{
+    class UnknownCommand : Command
+    {
+        public UnknownCommand(Repository repository, string[] param) : base(repository, param)
+        {
+
+        }
+
+        public override string Execute()
+        {
+            return "Вы ввели неизвестную команду!";
+        }
+    }
+}
